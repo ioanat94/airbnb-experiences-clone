5304ac53e766ed3806b3fe44f115d775a6e6e28e
ioanat94 <ioanatiplea94@gmail.com>
2022-05-03 09:54:07 +0300

    Added props to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
-import cardImage from '../images/katie-zaferes.png';
 import starImage from '../images/star.png';
 
-function Card() {
+function Card(props) {
     return (
         <div className='card'>
-            <img src={cardImage} alt='Katie Zaferes' className='card-img' />
+            <img src={require(`../images/${props.img}`)} alt='Katie Zaferes' className='card-img' />
             <div className='card-stats'>
                 <img src={starImage} alt='Star' className='star-img' />
-                <span>5.0</span>
-                <span className='gray'>(6) • </span>
-                <span className='gray'>USA</span>
+                <span>{props.rating}</span>
+                <span className='gray'>({props.reviewCount}) • </span>
+                <span className='gray'>{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className='bold'>From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     )
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -5,7 +5,14 @@ function Content() {
     return (
         <div className='content'>
             <Hero />
-            <Card />
+            <Card 
+                img='katie-zaferes.png' 
+                rating='5.0'
+                reviewCount={6}
+                country='USA'
+                title='Life Lessons with Katie Zaferes'
+                price={136}
+            />
         </div>
     )
 }
